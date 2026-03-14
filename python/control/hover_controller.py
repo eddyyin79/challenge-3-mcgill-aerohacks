b3989b1b38977784b1d1770eb1e6199f7789f129
Sebastian Inouye <sebastian.inouye@mail.mcgill.ca>
2026-03-14 18:00:50 -0400

    deleate the yaw part

--- python/control/hover_controller.py
+++ python/control/hover_controller.py
@@ -16,10 +16,6 @@ def stabilize_drone(self, speed):
         drone.manual_thrusts(speed, speed - 1, speed , speed - 1)
     if drone.get_roll() < -0.5:
         drone.manual_thrusts(speed, speed + 1, speed , speed + 1)
-    if drone.get_yaw() > 0.5:
-        drone.manual_thrusts(speed +1, speed - 1, speed +1, speed - 1)
-    if drone.get_yaw() < -0.5:
-        drone.manual_thrusts(speed - 1, speed + 1, speed - 1, speed + 1)
 
     #a front left, b front right, c back left, d back right
     #drone.manual_thrusts(a, b, c, d)
@@ -28,7 +24,6 @@ def stabilize_drone(self, speed):
         # Calculate errors
         pitch_error = target_pitch - drone.get_pitch()
         roll_error = target_roll - drone.get_roll()
-        yaw_error = target_yaw - drone.get_yaw()
 
         # Estimate vertical thrust component
         # total_thrust should be the sum of all motor thrusts (define as needed)
@@ -49,14 +44,13 @@ def stabilize_drone(self, speed):
         # Calculate control outputs (this is a simplified example)
         pitch_control = 1 * pitch_error  # P term only
         roll_control = 1 * roll_error    # P term only
-        yaw_control = 1 * yaw_error      # P term only
         altitude_control = 1 * altitude_error  # P term only
 
         # Adjust motor speeds based on control outputs
-        a = altitude_control + pitch_control + roll_control + yaw_control
-        b = altitude_control + pitch_control - roll_control - yaw_control
-        c = altitude_control - pitch_control + roll_control - yaw_control
-        d = altitude_control - pitch_control - roll_control + yaw_control
+        a = altitude_control + pitch_control + roll_control 
+        b = altitude_control + pitch_control - roll_control 
+        c = altitude_control - pitch_control + roll_control
+        d = altitude_control - pitch_control - roll_control
 
         total_thrust = a + b + c + d  # Update total thrust based on control outputs
         drone.manual_thrusts(a, b, c, d)
